aa6970bec6cefffbe72b6ec367f3f360484dbc14
Frédéric Forjan <frederic.forjan@aveva.com>
2022-09-29 14:17:42 +0200

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+namespace my_issue;
+public class Class1
+{
+    public static void ExampleMethod(byte[] rgbIV)
+    {
+        var aesCng  = new AesCng();
+        aesCng.IV = rgbIV;
+        aesCng.CreateEncryptor();
+    }
+}
